8ddfc370188855ad1b2fed1e548c50fc22163fc7
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2022-11-02 15:47:14 +0700

    Add home page

---
+++ resources/views/components/product-carousel.blade.php
@@ -0,0 +1,17 @@
+<div class="bg-white w-3/4 mx-auto pb-4">
+    <div class="flex gap-4 p-4 bg-gray-400">
+        <h2>Beauty</h2>
+        <a href="#" class="text-blue-600 hover:underline">View All</a>
+    </div>
+    <hr class="opacity-20">
+
+    <div class="flex bg-white overflow-x-scroll mt-4 gap-4 px-4 pt-2 pb-4">
+        <x-product-card />
+        <x-product-card />
+        <x-product-card />
+        <x-product-card />
+        <x-product-card />
+        <x-product-card />
+        <x-product-card />
+    </div>
+</div>
